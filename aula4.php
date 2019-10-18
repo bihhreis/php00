24d0a58861b09e7cb651ad5565cdb812b3b859c6
SaturnDraco61 <56743834+SaturnDraco61@users.noreply.github.com>
2019-10-18 20:26:57 -0300

    erro
    ajudem a solucionar

---
+++ aula4.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Média dos Alunos</title>
+</head>
+<body>
+    <h1>Calcula a Média dos Alunos</h1>
+    <hr>
+    
+    <form method="POST">
+        
+        Nome do Aluno:
+        <br>
+        <input type="text" name="txtnome" > <br>
+        <br>
+
+        Nota1:
+        <br>
+        <input type="text" name="txtnota1"> <br>
+        <br>
+    
+        Nota2:
+        <br>
+        <input type="text" name="txtnota2"> <br>
+        <br>
+
+        Nota3:
+        <br>
+        <input type="text" name="txtnota3"> <br>
+        <br>
+        
+        <input type="submit" name="btncalcular" value="Calcular Média" >
+        
+    </form>
+
+    <?php
+    
+        if ($_POS) {
+        
+            //Recebe dados do formulário
+            $nome=$_POST['txtnome'];
+            $nota1=$_POST['txtnota1'];
+            $nota2=$_POST['txtnota2'];
+            $nota3=$_POST['txtnota3'];
+
+            // Calcula Média
+            $media = ($nota1+$nota2+$nota3)/3;
+            
+
+            // Mostrando Resultado
+            
+            echo "<br> <hr> A Média do Aluno $nome é $media";
+            echo "<hr>";
+        
+        }
+    ?>
+
+</body>
+</html>
